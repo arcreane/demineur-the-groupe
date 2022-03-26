f9ae93c2371b98f0502fdaa4acdd7d6f82e8e062
marielaure1 <edjour.marielaure@gmail.com>
2022-03-27 00:40:07 +0100

    menu value error

---
+++ jeu.py
@@ -0,0 +1,38 @@
+
+# Création de la grille grille
+grille = []
+
+# Taille de la grille
+
+# Symbole
+carre = "■ "
+vide = "  "
+bombe = "💣️"
+
+def creationGrille(niveauColonne, niveauLigne):
+    # Ajout colonne et ligne + index
+    ligne = niveauLigne + 1
+    colonne = niveauColonne + 1
+    # Boucle 10 colonne
+    for i in range(colonne):
+        grille.append([])
+        # Boucle 10 ligne
+        for j in range(ligne):
+            # Pour chaque lignes, la première case correspond au numero de ligne
+            if(j == 0):
+                if(i < niveauColonne):
+                    grille[i].append(str(i).zfill(2))
+                else:
+                    grille[i].append(vide)
+            # Pour chaque colonnes, la dernière case correspond au numero de colonne
+            if(i == niveauColonne):
+                grille[i].append(str(j).zfill(2))
+            else:
+                grille[i].append(carre)
+    # Print de la grille
+    for i in range(colonne):
+        for j in range(ligne):
+            print(grille[i][j], end=" ")
+        print()
+
+
---
+++ menu.py
@@ -0,0 +1,91 @@
+import jeu
+# MENU
+
+# Accueil
+def accueil():
+    # Titre
+    for i in range(5):
+        if(i in [0, 4]):
+            print("💣️" + ("*" * 60) + "💣️")
+        elif(i in [1, 3]):
+            print("*" + 61* " " + "*")
+        elif(i == 2):
+            print("*" + " " * 14 + "💣️" + " Bienvenue sur le démineur "+ "💣️" + " " * 16 + "*")
+    print(" ")
+    # Options
+    choix1 = "1. Jouer"
+    choix2 = "2. Voir mes scores"
+    choix3 = "3. Quitter"
+    print("Choisissez une option : ")
+    print(choix1 + "\n" + choix2 + "\n" + choix3 + "\n")
+    joueur = input("Quel est votre choix ? ")
+    print(" ")
+    while(True):
+        try: 
+            if(int(joueur) not in [1, 2, 3]):
+                print("Veuillez saisir le chiffre correspondant à votre choix")
+                joueur = input("Quel est votre choix ? ")
+            else:
+                break
+        except ValueError:
+            print("Veuillez saisir le chiffre correspondant à votre choix")
+            joueur = input("Quel est votre choix ? ")
+        print(" ")
+    if(int(joueur) == 1):
+        niveauChoix()
+    elif(int(joueur) == 2):
+        scores()
+    elif(int(joueur) == 3):
+        quitter()
+        
+
+# Niveaux
+def niveauChoix():
+    # Proposer niveau ou retour au menu
+    print("Choisissez un niveau de difficulté : ")
+    niveau1 = "1. Facile"
+    niveau2 = "2. Normal"
+    niveau3 = "3. Difficile"
+    retour = "4. Retourner au Menu"
+    print(niveau1 + "\n" + niveau2 + "\n" + niveau3 + "\n" + retour + "\n")
+    joueur = input("Quel est votre choix ? ")
+    print(" ")
+    while(True):
+        try: 
+            if(int(joueur) not in [1, 2, 3, 4]):
+                print("Veuillez saisir le chiffre correspondant à votre choix")
+                joueur = input("Quel est votre choix ? ")
+            else:
+                break
+        except ValueError:
+            print("Veuillez saisir le chiffre correspondant à votre choix")
+            joueur = input("Quel est votre choix ? ")
+        print(" ")
+    if(int(joueur) == 1):
+        niveauColonne = 9
+        niveauLigne = 9
+        print("Vous avez choisie le niveau : Facile\n")
+        jeu.creationGrille(niveauColonne, niveauLigne)
+    elif(int(joueur) == 2):
+        niveauColonne = 16
+        niveauLigne = 16
+        print("Vous avez choisie le niveau : Normal\n")
+        jeu.creationGrille(niveauColonne, niveauLigne)
+    elif(int(joueur) == 3):
+        niveauColonne = 16
+        niveauLigne = 30
+        print("Vous avez choisie le niveau : Difficile\n")
+        jeu.creationGrille(niveauColonne, niveauLigne)
+    elif(int(joueur) == 4):
+        accueil()
+
+# Scores
+def scores():
+    pass
+
+# Quitter
+def quitter():
+    pass
+
+
+accueil()
