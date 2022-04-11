e6495bdd4e0b002093d146988307f00725f879a3
marielaure1 <edjour.marielaure@gmail.com>
2022-04-11 12:22:14 +0200

    initialisation de la grille

--- accueil.py
+++ accueil.py
@@ -1,10 +1,39 @@
-import menu
+import jeu
 from tkinter import *
 
-# creer une fenetre
+# Création de la fenêtre Window
 window = Tk()
+window.title("Démineur")
+window.configure(bg = "#2515AA")
 
-menu.fenetre(window, 1, 9, 9, 10)
+# Création du Canvas
+canva = Canvas(window, width=300, height=300, bg='#2515AA')
+canva.pack()
+
+# MENU
+# Creation barre menu avec le widget Menu dans le parent window
+menuBar = Menu(window)
+
+# Ajout des catégories du menu
+# Jouer
+menuJouer = Menu(menuBar)
+menuBar.add_cascade(label = "Jouer", menu=menuJouer)
+# commandes jouer
+select = ""
+menuJouer.add_radiobutton(label = "Facile", value=1, variable=select, command=lambda: [jeu.initialisation(canva), jeu.creationGrille( 9, 9, 10)])
+menuJouer.add_radiobutton(label = "Normal", value=2, variable=select, command=lambda: [jeu.initialisation(canva), jeu.creationGrille(16, 16, 40)])
+menuJouer.add_radiobutton(label = "Difficile", value=3, variable=select, command=lambda: [jeu.initialisation(canva), jeu.creationGrille(30, 16, 99)])
+
+# Scores
+menuScores = Menu(menuBar)
+menuBar.add_command(label = "Scores") 
+
+# Quitter
+menuQuitter = Menu(menuBar)
+menuBar.add_command(label = "Quitter", command=quit)
+window.config(menu=menuBar)
+
+
 
 # Afficher
 window.mainloop()
--- jeu.py
+++ jeu.py
@@ -1,63 +1,129 @@
-import random
 from tkinter import *
+import random
 
 grille = []
 b = []
+presenceBombe = []
+canva = ""
+colonne = 0
+ligne = 0
+mines = 0
+numCase = 0
+maxCase = 0
 
-# grille
-def creationGrille(window, niveauColonne, niveauLigne, mines):
-    
-    canva = Canvas(window, width=300, height=300, bg='#2515AA')
-    numCase = 0
-    maxCase = niveauColonne * niveauLigne -1
+
+# Vider la grille
+def initialisation(canvas):
+    global grille, b, canva
+    canva = canvas
+    for i in grille:
+        i.destroy()
+    for j in b:
+        j.destroy()
+    canva.delete("all")
+
+# Creation de la grille
+def creationGrille(niveauColonne, niveauLigne, niveauMines):
+    global maxCase, numCase, colonne, ligne, mines
+    colonne = niveauColonne
+    ligne = niveauLigne
+    mines = niveauMines
+    maxCase = colonne * ligne - 1
+
     for i in range(niveauColonne):
         for j in range(niveauLigne):
             grille.append(Label(canva, text=" ", bd=1, width=1, height=1,padx=9,pady=5, relief=SUNKEN))
             grille[numCase].grid(row=j, column=i)
-
-            b.append(Button(canva,text="",image="",padx=8,pady=1)) 
-            b[numCase].grid(row=j, column=i)
-            b[numCase].bind("<Button-1>", lambda i, coord=numCase: click(grille, b, coord))
-            b[numCase].config(relief=RAISED)
-            numCase +=1
-    coordsMines(grille, maxCase, numCase, mines)
-    canva.pack()
     
+            # for i in range(mines):
+            #     nb1 = random.randint(0, maxCase)
+            #     while nb1 in presenceBombe:
+            #             nb1 = random.randint(0, maxCase)
+            #     presenceBombe.append(nb1)
+            #     grille[nb1].config(text="B", relief = GROOVE, bd=1,width=0,height=0)
+            #     presenceBombe.append(numCase)
+
+            # b.append(Button(canva,text="",image="",padx=8,pady=1)) 
+            # b[numCase].grid(row=j, column=i)
+            # b[numCase].bind("<Button-1>", lambda i, coord=numCase: click(coord))
+            # b[numCase].config(relief=RAISED)
+            
+            numCase +=1
+    # coordsMines()
+    
+
+# def coordsMines():
+#     global grille, maxCase,  mines, numCase
+#     presenceBombe = []
+    
+#     for i in range(mines):
+#         nb1 = random.randint(0, maxCase)
+#         while nb1 in presenceBombe:
+#                 nb1 = random.randint(0, maxCase)
+#         presenceBombe.append(nb1)
+#         grille[nb1].config(text="B", relief = GROOVE, bd=1,width=0,height=0)
+#         presenceBombe.append(numCase)
+
+    
+
+
+
+
+
+
+
 
 # photo= PhotoImage(file="img/bombe.png")
 
 # mines
-
-def click(grille, b, coord):
-    b[coord].grid_forget()
-    verifGagne(grille, coord)
-
-def coordsMines(grille, maxCase, numCase, mines):
-    presenceBombe = []
+# def click(coord):
+#     b[coord].grid_forget()
+#     # verifGagne(grille, coord)
     
-    for i in range(mines):
-        nb1 = random.randint(0, maxCase)
-        while nb1 in presenceBombe:
-                nb1 = random.randint(0, maxCase)
-        presenceBombe.append(nb1)
-        grille[nb1].config(text="B", relief = GROOVE, bd=1,width=0,height=0)
-        presenceBombe.append(numCase)
 
-def verifGagne(grille, coord):
-    if grille[coord].cget("text") == "B":
-        perdu()
-
-def perdu():
+# def coordsMines():
+#     global presenceBombe
     
-    perdu = Tk()
-    perdu.title("Démineur")
-    perdu.config(bg="#2515AA")
-    messagePerdu = Text(perdu, text = "BOUM! Vous avez perdu.", fg="Arial, 10")
-    messagePerdu.pack(expand=CENTER)
-    perdu.mainloop()
+#     for i in range(mines):
+#         nb1 = random.randint(0, maxCase)
+#         while nb1 in presenceBombe:
+#                 nb1 = random.randint(0, maxCase)
+#         presenceBombe.append(nb1)
+#         grille[nb1].config(text="B", relief = GROOVE, bd=1,width=0,height=0)
+#         presenceBombe.append(numCase)
+
+    # for i in presenceBombe:
+    #     nbVoisines = 0
+    #     for i in range(ligne-1, ligne+2):
+    #         for j in range(colonne-1, colonne+2):
+    #             nbVoisines += grille[i][j]
+    #             grille[j].config(text="1")
+ 
+
+# def verifGagne(grille, coord):
+#     if grille[coord].cget("text") == "B":
+#         perdu()
+
+# def perdu():
+    
+#     perdu = Tk()
+#     perdu.title("Démineur")
+#     perdu.config(bg="#2515AA")
+#     messagePerdu = Text(perdu, text = "BOUM! Vous avez perdu.", fg="Arial, 10")
+#     messagePerdu.pack(expand=CENTER)
+#     perdu.mainloop()
 
 
-# def supprimer(b, coord):
-#     maxCase = len(coord)
-#     for i in maxCase:
-#         b[i].grid_forget()
+# # def supprimer(b, coord):
+# #     maxCase = len(coord)
+# #     for i in maxCase:
+# #         b[i].grid_forget()
+
+
+
+
+
+# if not(i == 0 and j == 0):
+#     nbVoisines += grille[ligne + 1][colonne + j]
+# grille[ligne + 1][colonne + j].set("C")
+            
